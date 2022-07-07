afe7a961e793297fb025378d835fa65a2788f0f1
IsaiasSPinto <isaiascxs10@gmail.com>
2022-07-07 11:14:12 -0300

    modulo JamStack

---
+++ src/services/prismic.ts
@@ -0,0 +1,17 @@
+import * as prismic from '@prismicio/client'
+import sm from '../../sm.json'
+
+const endpoint = sm.apiEndpoint
+const repositoryName = prismic.getRepositoryName(endpoint)
+
+// This factory function allows smooth preview setup
+export function getPrismicClient() {
+  const client = prismic.createClient(
+    repositoryName,
+    {
+      accessToken : process.env.PRISMIC_ACCESS_TOKEN
+    }
+  )
+
+  return client
+}
